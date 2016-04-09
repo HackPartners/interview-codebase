e6d7a0acaf4b82a53ca0a407a197093324ea6b51
Alejandro Saucedo <axsauze@gmailcom>
2016-04-09 22:06:28 +0100

    removed typings

--- typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="main/ambient/angular/angular.d.ts" />
-/// <reference path="main/ambient/cordova-ionic/cordova-ionic.d.ts" />
-/// <reference path="main/ambient/cordova/cordova.d.ts" />
-/// <reference path="main/ambient/ionic/ionic.d.ts" />
-/// <reference path="main/ambient/jquery/jquery.d.ts" />
-/// <reference path="main/ambient/keyboard/keyboard.d.ts" />
